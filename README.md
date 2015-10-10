a30c56d7ca111b1e049da413cfb068fb8dd3c50b
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-10-10 07:53:42 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# alt-select-service
+[![Build Status](https://secure.travis-ci.org/dsn-nimbus/alt-select-service.png?branch=master)](https://travis-ci.org/dsn-nimbus/alt-select-service)
+[![Coverage Status](https://coveralls.io/repos/dsn-nimbus/alt-select-service/badge.svg?branch=master&service=github)](https://coveralls.io/r/dsn-nimbus/alt-select-service/?branch=master)
+
+## depedências
+
+- Angular 1.x;
+- Jquery 2.x;
+- Select2 4.x;
---
+++ tests/alt-select-service_test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+describe('Service: AltSelectService', function () {
+
+  var AltSelectService, _timeoutMock;
+  var TEMPO = 99;
+
+  beforeEach(module('alt.select-service'));
+
+  beforeEach(inject(function($injector) {
+    AltSelectService = $injector.get('AltSelectService');
+    _timeoutMock = $injector.get('$timeout');
+  }));
+
+  describe('criação', function() {
+    it('deve estar definido', function() {
+      expect(AltSelectService).toBeDefined();
+      expect(typeof AltSelectService).toBe("object");
+    })
+  })
+
+  describe('inicializar', function() {
+    it('deve chamar o inicializar corretamente - chamando o que é passado por parâmetro', function() {
+      var _id = 0;
+      var _opcoes = undefined;
+
+      spyOn($.fn, 'select2').and.callFake(angular.noop);
+
+      AltSelectService.inicializar(_id, _opcoes);
+
+      _timeoutMock.flush(TEMPO);
+
+      expect($('a').select2).toHaveBeenCalledWith(_opcoes);
+    })
+
+    it('deve chamar o inicializar corretamente - chamando o que é passado por parâmetro', function() {
+      var _id = 1;
+      var _opcoes = {a: true};
+
+      spyOn($.fn, 'select2').and.callFake(angular.noop);
+
+      AltSelectService.inicializar(_id, _opcoes);
+
+      _timeoutMock.flush(TEMPO);
+
+      expect($('a').select2).toHaveBeenCalledWith(_opcoes);
+    })
+  })
+});
