f98bdb734f05316e11b2391df1e136be34b23a50
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-02-23 09:05:07 -0300

    adiciona metodo para abertura do select2 atraves de script

--- src/alt-select-service.js
+++ src/alt-select-service.js
@@ -4,13 +4,21 @@
   angular
     .module('alt.select-service', [])
     .service('AltSelectService', ['$timeout', function($timeout) {
-      this.inicializar = function(id, opt) {
+      var TIMEOUT = 33;
+
       // $timeout: necessário para remover problema de tempo de execução entre o angular e o select2
       // antes da modificação, por mais que o select estivesse selecionado no html, o select2 não atualizava
 
+      this.inicializar = function(id, opt) {
         $timeout(function() {
           $(id).select2(opt);
-        }, 33);
+        }, TIMEOUT);
       };
+
+      this.abrir = function(id, opt) {
+        $timeout(function() {
+          $(id).select2('open', opt);
+        }, TIMEOUT);
+      }
   }]);
 }());
--- tests/alt-select-service_test.js
+++ tests/alt-select-service_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 describe('Service: AltSelectService', function () {
-
   var AltSelectService, _timeoutMock;
   var TEMPO = 99;
 
@@ -44,6 +43,34 @@ describe('Service: AltSelectService', function () {
       _timeoutMock.flush(TEMPO);
 
       expect($('a').select2).toHaveBeenCalledWith(_opcoes);
+    })
+  })
+
+  describe('abrir', function() {
+    it('deve chamar o abrir corretamente - chamando o que é passado por parâmetro', function() {
+      var _id = 0;
+      var _opcoes = undefined;
+
+      spyOn($.fn, 'select2').and.callFake(angular.noop);
+
+      AltSelectService.abrir(_id, _opcoes);
+
+      _timeoutMock.flush(TEMPO);
+
+      expect($('a').select2).toHaveBeenCalledWith('open', _opcoes);
+    })
+
+    it('deve chamar o inicializar corretamente - chamando o que é passado por parâmetro', function() {
+      var _id = 1;
+      var _opcoes = {a: true};
+
+      spyOn($.fn, 'select2').and.callFake(angular.noop);
+
+      AltSelectService.abrir(_id, _opcoes);
+
+      _timeoutMock.flush(TEMPO);
+
+      expect($('a').select2).toHaveBeenCalledWith('open', _opcoes);
     })
   })
 });
