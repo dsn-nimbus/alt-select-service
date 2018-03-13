858bc3211da7de8ebcb55760096d8ac681400db2
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-03-13 15:26:35 -0300

    .

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import coveralls from 'gulp-coveralls';
-import cssmin from 'gulp-cssmin';
 import concat from 'gulp-concat';
 import rename from 'gulp-rename';
 import {Server as Karma} from 'karma';
@@ -9,21 +8,11 @@ import babel from 'gulp-babel';
 
 const _coverage = 'coverage/**/lcov.info';
 const _scripts = 'src/**/*.js';
-const _styles = 'src/**/*.css';
 const _script = 'alt-select-service.js';
 const _style = 'alt-select-service.css';
 const _dist = 'dist';
 
-gulp.task('build-css', function () {
-  return gulp.src(_styles)
-    .pipe(concat(_style.toLowerCase()))
-    .pipe(gulp.dest(_dist))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(_dist));
-})
-
-gulp.task('build', ['unit_test', 'build-css'], function () {
+gulp.task('build', ['unit_test'], function () {
   return gulp.src(_scripts)
     .pipe(concat(_script.toLowerCase()))
     .pipe(gulp.dest(_dist))
@@ -39,13 +28,11 @@ gulp.task('build', ['unit_test', 'build-css'], function () {
 })
 
 gulp.task('unit_test', function (done) {
-  let _opts = {
+  return new Karma({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true,
     browsers: ['Chrome']
-  };
-
-  return new Karma(_opts, done).start();
+  }, (exitCode) => done(exitCode)).start();
 })
 
 gulp.task('coverage', ['unit_test'], function () {
